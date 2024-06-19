226f6157664705a6d586805332a83b64d5a09905
noite-hunter <140098495+noite-hunter@users.noreply.github.com>
2024-06-19 02:34:37 -0300

    Removido scanner da main pois não era mais utilizado

--- src/main/java/com/poo/introducao/IntroducaoApplication.java
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -2,7 +2,6 @@ package com.poo.introducao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class IntroducaoApplication {
