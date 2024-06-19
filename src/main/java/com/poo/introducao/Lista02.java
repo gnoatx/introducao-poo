6d602ca9b3c99b3076624d9502259a3da8eaccab
noite-hunter <140098495+noite-hunter@users.noreply.github.com>
2024-06-19 01:57:43 -0300

    Exercício 05 feito

--- src/main/java/com/poo/introducao/Lista02.java
+++ src/main/java/com/poo/introducao/Lista02.java
@@ -54,7 +54,7 @@ public class Lista02 {
         String nome;
         int idade;
         char pcd, gestante;
-        String mensagemPreferencial = ", você tem direito a ir para a fila preferencial.";
+        final String MENSAGEM_PREFERENCIAL = ", você tem direito a ir para a fila preferencial.";
 
         System.out.println("===== Fila de Espera =====\n" +
                            "Este programa fará 3 perguntas para decidir se você vai para a fila comum ou preferencial.\n");
@@ -66,7 +66,7 @@ public class Lista02 {
         idade = scInt.nextInt();
         scInt.close();
         if (idade >= 65) {
-            System.out.println(nome + mensagemPreferencial);
+            System.out.println(nome + MENSAGEM_PREFERENCIAL);
             scChar.close();
             return;
         }
@@ -74,7 +74,7 @@ public class Lista02 {
         System.out.print("Você é PCD? (s/n): ");
         pcd = scChar.next().charAt(0);
         if (pcd == 's') {
-            System.out.println(nome + mensagemPreferencial);
+            System.out.println(nome + MENSAGEM_PREFERENCIAL);
             scChar.close();
             return;
         }
@@ -83,7 +83,7 @@ public class Lista02 {
         gestante = scChar.next().charAt(0);
         scChar.close();
         if (gestante == 's') {
-            System.out.println(nome + mensagemPreferencial);
+            System.out.println(nome + MENSAGEM_PREFERENCIAL);
             return;
         }
 
@@ -91,6 +91,31 @@ public class Lista02 {
     }
 
     void ex4() {
+        Scanner scString = new Scanner(System.in);
+        Scanner scInt = new Scanner(System.in);
 
+        String nome;
+        int idade;
+
+        System.out.println("===== Idade de Entrada =====\n" +
+                           "Este programa recebe a idade de um cliente e retorna se este pode ou não entrar num evento.\n");
+        System.out.print("Digite seu nome: ");
+        nome = scString.nextLine();
+        scString.close();
+        System.out.println("Digite sua idade: ");
+        idade = scInt.nextInt();
+        scInt.close();
+
+        if (idade < 16) {
+            System.out.println(nome + ", você não pode participar neste evento.");
+        } else if (idade < 18) {
+            System.out.println(nome + ", você apenas poderá participar neste evento com acompanhamento de responsáveis.");
+        } else {
+            System.out.println(nome + ", você pode participar neste evento!");
+        }
+    }
+
+    void ex5() {
+        
     }
 }
