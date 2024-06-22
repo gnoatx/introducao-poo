0e746cb697d9dda7fc01da140fc10292d8f0b209
noite-hunter <140098495+noite-hunter@users.noreply.github.com>
2024-06-21 21:21:39 -0300

    Separada função recebeVendas para reutilizar em vários exercícios

--- src/main/java/com/poo/introducao/Lista04.java
+++ src/main/java/com/poo/introducao/Lista04.java
@@ -6,15 +6,12 @@ import java.util.ArrayList;
 @SuppressWarnings("resource")
 public class Lista04 {
 
-    void ex1() {
+    public ArrayList<Double> vendas = new ArrayList<Double>();
+
+    void recebeVendas() {
         Scanner scDouble = new Scanner(System.in);
         Scanner scChar = new Scanner(System.in);
-        ArrayList<Double> vendas = new ArrayList<Double>();
         char continua;
-        double soma = 0;
-
-        System.out.println("===== Fechamento do Dia =====\n" +
-                           "Este programa recebe uma lista de valores numa ArrayList e faz a soma desta.\n");
 
         do {
             System.out.print("Digite o valor da venda: ");
@@ -23,7 +20,15 @@ public class Lista04 {
             System.out.print("Adicionar mais um item? (s/n): ");
             continua = scChar.nextLine().charAt(0);
         } while (continua == 's');
+    }
 
+    void ex1() {
+        double soma = 0;
+
+        System.out.println("===== Fechamento do Dia =====\n" +
+                           "Este programa recebe uma lista de valores numa ArrayList e faz a soma desta.\n");
+
+        recebeVendas();
         for (Double d : vendas) {
             soma += d;
         }
